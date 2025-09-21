ab607847d87792156decaa3fd2180b70ec5fbd67
alirez khayatpour <alireza_khayatpour@yahoo.com>
2025-09-21 16:38:54 +0330

    realstate-next: rename Home to HomeCard

--- components/templates/index/Homes.tsx
+++ components/templates/index/Homes.tsx
@@ -1,15 +1,17 @@
-import Home from "@/components/modules/Home";
+import HomeCard from "@/components/modules/HomeCard";
 import React from "react";
 import db from "../../../data/db.json";
 
 function Homes() {
 	console.log("db: ", db);
 	const homeLists = db.homes;
-	return <div className='homes'>
-    {
-      db.homes.slice(0,6).map(home => <Home key={home.id} {...home} /> )
-    }
-    </div>;
+	return (
+		<div className='homes'>
+			{db.homes.slice(0, 6).map((home) => (
+				<HomeCard key={home.id} {...home} />
+			))}
+		</div>
+	);
 }
 
 export default Homes;
