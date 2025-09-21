649b741e8a086a844e95f21c8502acf8d644669a
alirez khayatpour <alireza_khayatpour@yahoo.com>
2025-09-21 13:05:06 +0330

    realstate-next: add Home Component

---
+++ components/modules/Home.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function Home() {
+  return (
+		<div className='card'>
+			<img src='img/gal-1.jpeg' alt='House 6' className='card__img' />
+			<h5 className='card__title'>آپارتمان خانوادگی مدرن</h5>
+			<span className='card__like'>
+				<i className='fa fa-heart'></i>
+			</span>
+			<div className='card__details'>
+				<span className=''>
+					<i className='fa fa-map-marker card__icon'></i>
+				</span>
+				<p className='card__text'>مالدیو</p>
+				<span className=''>
+					<i className='fa fa-user card__icon'></i>
+				</span>
+				<p className='card__text'>4 اتاق</p>
+				<span className=''>
+					<i className='fa fa-expand card__icon'></i>
+				</span>
+				<p className='card__text'>400 متر مربع</p>
+				<span className=''>
+					<i className='fa fa-key card__icon'></i>
+				</span>
+				<p className='card__text'>560 میلیون تومان</p>
+			</div>
+
+			<a href='#' className='btn btn-brown btn-card'>
+				مشاهده ملک
+			</a>
+		</div>
+	);
+}
+
+export default Home
