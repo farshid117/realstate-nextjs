16b3afdb1029de4eb12134e750e7f2e81511d87f
alirez khayatpour <alireza_khayatpour@yahoo.com>
2025-09-21 15:16:30 +0330

    realstate-next: adjust  Home& Homes Components

--- components/modules/Home.tsx
+++ components/modules/Home.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
 
-function Home() {
-  return (
+function Home({ id, price, title, img, meterage, roomCount }) {
+	return (
 		<div className='card'>
-			<img src='img/gal-1.jpeg' alt='House 6' className='card__img' />
-			<h5 className='card__title'>آپارتمان خانوادگی مدرن</h5>
+			<img src={img} alt='House 6' className='card__img' />
+			<h5 className='card__title'> {title} </h5>
 			<span className='card__like'>
 				<i className='fa fa-heart'></i>
 			</span>
@@ -12,19 +11,19 @@ function Home() {
 				<span className=''>
 					<i className='fa fa-map-marker card__icon'></i>
 				</span>
-				<p className='card__text'>مالدیو</p>
+				<p className='card__text'>شیراز</p>
 				<span className=''>
 					<i className='fa fa-user card__icon'></i>
 				</span>
-				<p className='card__text'>4 اتاق</p>
+				<p className='card__text'>{roomCount.toLocaleString("fa")} اتاق</p>
 				<span className=''>
 					<i className='fa fa-expand card__icon'></i>
 				</span>
-				<p className='card__text'>400 متر مربع</p>
+				<p className='card__text'>{meterage.toLocaleString("fa")} متر مربع</p>
 				<span className=''>
 					<i className='fa fa-key card__icon'></i>
 				</span>
-				<p className='card__text'>560 میلیون تومان</p>
+				<p className='card__text'>{price.toLocaleString("fa")} میلیون تومان</p>
 			</div>
 
 			<a href='#' className='btn btn-brown btn-card'>
--- components/templates/index/Homes.tsx
+++ components/templates/index/Homes.tsx
@@ -1,12 +1,15 @@
 import Home from "@/components/modules/Home";
 import React from "react";
+import db from "../../../data/db.json";
 
 function Homes() {
-  return (
-    <div className="homes">
-      <Home/>
-    </div>
-  );
+	console.log("db: ", db);
+	const homeLists = db.homes;
+	return <div className='homes'>
+    {
+      db.homes.map(home => <Home key={home.id} {...home} /> )
+    }
+    </div>;
 }
 
 export default Homes;
