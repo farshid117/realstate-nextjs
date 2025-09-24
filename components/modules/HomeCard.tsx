4e2270ee8f01f278c60db62ec31554f7a838c381
alirez khayatpour <alireza_khayatpour@yahoo.com>
2025-09-24 13:35:16 +0330

    realstate-next --> HomesCard : add catepory to card

--- components/modules/HomeCard.tsx
+++ components/modules/HomeCard.tsx
@@ -1,16 +1,23 @@
 import Image from "next/image";
 function HomeCard({
 	id,
-	price,
 	title,
 	img,
-	meterage,
 	roomCount,
 	address,
+	meterage,
+	price,
+	category,
 }: any) {
 	return (
 		<div className='card'>
-			<Image src={img} alt={title || "تصویر ملک"} width={100} height={100}  className='card__img ' />
+			<Image
+				src={img}
+				alt={title || "تصویر ملک"}
+				width={100}
+				height={100}
+				className='card__img '
+			/>
 			<h5 className='card__title'> {title} </h5>
 			<span className='card__like'>
 				<i className='fa fa-heart'></i>
@@ -38,6 +45,10 @@ function HomeCard({
 				<p className='card__text font-bold'>
 					{price.toLocaleString("fa")} میلیون تومان
 				</p>
+				<span className=''>
+					<i className='fa fa-key card__icon'></i>
+				</span>
+				<p className='card__text font-bold'> دسته بندی : {category} </p>
 			</div>
 
 			<a href={`/homes/${id}`} className='btn btn-brown btn-card'>
