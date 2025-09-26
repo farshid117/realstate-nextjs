4a1d70f451f2390253f4543c1c11c8e9ce38a698
alirez khayatpour <alireza_khayatpour@yahoo.com>
2025-09-26 14:44:13 +0330

    realstate-next --> page/index.tsx: add Head Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,23 @@ import Features from "@/components/sections/index/Features";
 import Gallery from "@/components/sections/index/Gallery";
 import Homes from "@/components/sections/index/Homes";
 import Story from "@/components/sections/index/Story";
-
+import Head from "next/head";
 
 function index() {
 	return (
 		<>
-    <Features/>
-    <Story/>
-    <Homes/>
-    <Gallery/>
-
-    </>
+			<Head>
+				<title>صفحه اصلی - پروژه املاک</title>
+				<meta
+					name='description'
+					content='سایت خرید و فروش و اجاره املاک با بهترین قیمت‌ها'
+				/>
+			</Head>
+			<Features />
+			<Story />
+			<Homes />
+			<Gallery />
+		</>
 	);
 }
 
